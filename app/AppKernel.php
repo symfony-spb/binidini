a88f2b699e2c20e4b081fa34cbfac6141b72ec52
Denis Manilo <d.manilo@iconto.ru>
2014-10-15 18:43:54 +0400

    Binidini first commit

---
+++ app/AppKernel.php
@@ -0,0 +1,18 @@
+<?php
+
+use Symfony\Component\ClassLoader\DebugUniversalClassLoader;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Debug\ErrorHandler;
+use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
+use Sylius\Bundle\CoreBundle\Kernel\Kernel;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+        );
+
+        return array_merge($bundles, parent::registerBundles());
+    }
+}
