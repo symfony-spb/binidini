9db45215c4babf2a6e65ac14a41455d0ac3c14a2
Denis Manilo <d.manilo@iconto.ru>
2014-10-20 16:43:38 +0400

    small changes

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,9 +1,5 @@
 <?php
 
-use Symfony\Component\ClassLoader\DebugUniversalClassLoader;
-use Symfony\Component\Config\Loader\LoaderInterface;
-use Symfony\Component\HttpKernel\Debug\ErrorHandler;
-use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
 use Sylius\Bundle\CoreBundle\Kernel\Kernel;
 
 class AppKernel extends Kernel
@@ -11,6 +7,7 @@ class AppKernel extends Kernel
     public function registerBundles()
     {
         $bundles = array(
+            new Bini\Bundle\WebBundle\BiniWebBundle(),
         );
 
         return array_merge($bundles, parent::registerBundles());
